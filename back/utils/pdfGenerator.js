f18d87bbb48ceb8da5081a774c1424f065d7efc9
Tirumala Raju Mahali <103620129+Tirumala02@users.noreply.github.com>
2024-12-17 13:57:42 +0530

    Update pdfGenerator.js

--- back/utils/pdfGenerator.js
+++ back/utils/pdfGenerator.js
@@ -72,7 +72,7 @@ function convertToIndianWords(number) {
 async function fetchHSNFromPython(query) {
     console.log("fetchHSNFromPython: " + query)
     try {
-        const response = await axios.post('https://xchange-hsn-code.onrender.com', {
+        const response = await axios.post('https://xchange-hsn-code.onrender.com/get-hsn-code', {
             query: query  // Send the query as JSON in the request body
         });
         console.log("response.data");  // Log the response to inspect it
